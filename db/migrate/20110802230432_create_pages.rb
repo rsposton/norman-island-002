b774b0f5cb9544c259e1c922861443585d750726
Regan Poston <regan@milyoni.com>
2011-08-02 16:28:34 -0700

    fix to rake

--- db/migrate/20110802230432_create_pages.rb
+++ db/migrate/20110802230432_create_pages.rb
@@ -2,7 +2,7 @@ class CreatePages < ActiveRecord::Migration
   def self.up
     create_table :pages do |t|
       t.string "name"
-      t.permalink "string"
+      t.string "permalink"
       t.integer "position"
       t.boolean "visible", :default => false
 
