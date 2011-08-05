4cd853bbce1d7b434a724f96bd9d68d1f77ef12c
Regan Poston <regan@milyoni.com>
2011-08-05 16:15:23 -0700

    initial list and show for subjects controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,2 +1,16 @@
 class SubjectsController < ApplicationController
+
+  def index
+    list
+    render('list')
+  end
+
+  def list
+    @subjects = Subject.order("subjects.position ASC")
+  end
+
+  def show
+    @subject = Subject.find(params[:id])
+  end
+
 end
