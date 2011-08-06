df44b6dc640aee6a6aa5665fc03a6cb152780f92
Regan Poston <regan@milyoni.com>
2011-08-05 18:53:30 -0700

    finished the subjects views - good times

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -13,4 +13,42 @@ class SubjectsController < ApplicationController
     @subject = Subject.find(params[:id])
   end
 
+  def new
+    @subject = Subject.new
+  end
+
+  def create
+    @subject = Subject.new(params[:subject])
+    if @subject.save
+      flash[:notice] = "Subject created."
+      redirect_to(:action => 'list')
+    else
+      render('new')
+    end
+  end
+
+  def edit
+    @subject = Subject.find(params[:id]) 
+  end
+
+  def update
+    @subject = Subject.find(params[:id])
+    if @subject.update_attributes(params[:subject])
+      flash[:notice] = "Subject updated."
+      redirect_to(:action => 'show', :id => @subject.id)
+    else
+      render('edit')
+    end
+  end
+
+  def delete
+    @subject = Subject.find(params[:id]) 
+  end
+
+  def destroy
+    Subject.find(params[:id]).destroy
+    flash[:notice] = "Subject destroyed."
+    redirect_to(:action => 'list')
+  end
+
 end
